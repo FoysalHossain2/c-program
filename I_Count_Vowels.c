957297ae305e92cda98595428e6704ccaeab4b54
FoysalHossain2 <fh375170@gmail.com>
2026-01-20 00:22:05 +0600

    Done

--- I_Count_Vowels.c
+++ I_Count_Vowels.c
@@ -4,10 +4,16 @@ int rec(char s[], int i)
 {
     if (s[i]=='\0')
     {
-        return;
+        return 0;
     }
    int cnt = rec(s,i+1);
-   return cnt+1;
+   if (s[i]== 'a' || s[i]== 'e' || s[i]== 'i' || s[i]== 'o' || s[i]== 'u' || s[i]== 'A' || s[i]== 'E' || s[i]== 'I' || s[i]== 'O' || s[i]== 'U')
+   {
+        return cnt+1;
+   }
+   else {
+        return cnt;
+   }   
     
 }
 
@@ -16,7 +22,8 @@ int main()
     char s[201];
     fgets(s, 201, stdin);
     int ans = rec(s,0);
-    printf("%d", ans);
+    printf("%d ", ans);
+    
     return 0;
     
 }
