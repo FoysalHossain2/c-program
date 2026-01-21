98767f06fa994c6549c49432de1673bc413c223b
FoysalHossain2 <fh375170@gmail.com>
2026-01-22 00:55:47 +0600

    Max Number solve

--- K_Max_Number.c
+++ K_Max_Number.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int maxNum (int n) {
-    int max = 
+int maxNum (int n, int a[]) {
+    int max = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i]; 
+        }
+    }
+
+    return max;
+    
 }
 
 int main()
@@ -14,7 +24,8 @@ int main()
         scanf("%d", &a[i]);
     }
     
-    maxNum(n);
+    int ans = maxNum(n, a);
+    printf("%d", ans);
 
     return 0;
 }
