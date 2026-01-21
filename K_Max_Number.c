a96e613024871968f2284218d780643413af6def
FoysalHossain2 <fh375170@gmail.com>
2026-01-21 23:41:11 +0600

    Max Number Using C

---
+++ K_Max_Number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int maxNum (int n) {
+    int max = 
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    maxNum(n);
+
+    return 0;
+}
