a0cd603e8194bee39d2d42d13a4e60754a267a16
FoysalHossain2 <fh375170@gmail.com>
2026-01-25 22:51:18 +0600

    done

--- Find_the_Missing_Number.c
+++ Find_the_Missing_Number.c
@@ -2,5 +2,31 @@
 
 int main()
 {
-    
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        long long M, A, B, C;
+        scanf("%lld %lld %lld %lld", &M, &A, &B, &C);
+
+        long long messingNum = A * B * C;
+
+        if (messingNum == 0)
+        {
+            if (M == 0)
+                printf("0\n");
+            else
+                printf("-1\n");
+        }
+        else
+        {
+            if (M % messingNum == 0)
+                printf("%lld\n", M / messingNum);
+            else
+                printf("-1\n");
+        }
+    }
+
+    return 0;
 }
