f08ebd8adb93bb832bc2061878685bc2654f69c0
FoysalHossain2 <fh375170@gmail.com>
2026-01-29 01:24:18 +0600

    jadu Matrix

--- Jadu_Matrix.c
+++ Jadu_Matrix.c
@@ -2,27 +2,57 @@
 
 int main()
 {
-    int n,m;
+    int n, m;
     scanf("%d %d", &n, &m);
+
     int a[n][m];
 
-    for ( int i = 0; i <n ; i++)
+    for (int i = 0; i < n; i++)
     {
-        for ( int j = 0; j <m ; j++)
+        for (int j = 0; j < m; j++)
         {
             scanf("%d", &a[i][j]);
         }
     }
-    
-    for ( int i = 0; i <n ; i++)
-    {
-        for ( int j = 0; j <m ; j++)
-        {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
-    }
-    
-    return 0;
 
+    // Step 1: Must be square
+    if (n != m)
+    {
+        printf("NO");
+        return 0;
+    }
+
+    int flag = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (i == j || i + j == n - 1)
+            {
+                if (a[i][j] != 1)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            else
+            {
+                if (a[i][j] != 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+
+    if (flag == 1)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
 }
