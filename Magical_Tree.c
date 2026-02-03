d8f794723770235e2cb7ce84c05a5160419dd482
FoysalHossain2 <fh375170@gmail.com>
2026-02-04 01:50:36 +0600

    Magical Tree

--- Magical_Tree.c
+++ Magical_Tree.c
@@ -4,31 +4,38 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int star = 1;
-    int space = n-1;
 
-    for (int i = 1; i <= n; i++)
+    int maxStar = n + 10;                 
+    int topRows = (maxStar + 1) / 2;     
+
+    int star = 1;
+    int space = topRows - 1;
+
+    for (int i = 1; i <= topRows; i++)
     {
         for (int j = 1; j <= space; j++)
-        {
             printf(" ");
-        }
 
         for (int j = 1; j <= star; j++)
-        {
-            printf("*"); 
-        }
+            printf("*");
+
         printf("\n");
         star += 2;
         space--;
-       
-        if (condition)
-        {
-            /* code */
-        }
-        
     }
-    
+
+    int trunkSpace = topRows - (n + 1) / 2;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= trunkSpace; j++)
+            printf(" ");
+
+        for (int j = 1; j <= n; j++)
+            printf("*");
+
+        printf("\n");
+    }
 
     return 0;
 }
